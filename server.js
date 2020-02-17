40860b463f37ea7235ddc7e8f9a10a87f298c92a
Keith Stevens <mrkeith782@gmail.com>
2020-02-16 22:11:00 -0800

    working a bit more on views

--- server.js
+++ server.js
@@ -2,22 +2,25 @@
 //and try to reduce some of the redudancies later.
 
 var fs = require('fs');
-// var path = require('path');
+var path = require('path');
 var express = require('express');
-// var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 var express_handlebars = require('express-handlebars'); 
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.engine('handlebars', express_handlebars({defaultLayout: 'skeleton'}));
+app.set('view engine', 'handlebars');
+
 //Main webpage
 app.get('/', function (req, res, next) {
-    res.render('skeleton');
+    res.render('404');
     res.status(200);
 });
 
 //404 Page
 app.get('*', function (req, res, next) {
-    res.render('404');
+    res.render('partials/404');
     res.status(400);
 });
 
